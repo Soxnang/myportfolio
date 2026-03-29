ed79ecf97e743477be0b0f0904ad09934fde1c7f
Soxnang <snac2214@gmail.com>
2026-03-29 22:51:22 +0000

    feat: infrastructure Terraform pour MyPortfolio

---
+++ terraform/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_s3_bucket" "portfolio" {
+  bucket = "myportfolio-soxnang-terraform"
+}
+
+resource "aws_s3_bucket_website_configuration" "portfolio" {
+  bucket = aws_s3_bucket.portfolio.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "portfolio" {
+  bucket = aws_s3_bucket.portfolio.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "portfolio" {
+  bucket = aws_s3_bucket.portfolio.id
+  depends_on = [aws_s3_bucket_public_access_block.portfolio]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.portfolio.arn}/*"
+    }]
+  })
+}
+
+output "website_url" {
+  value = aws_s3_bucket_website_configuration.portfolio.website_endpoint
+}
